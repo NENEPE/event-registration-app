b8b33168fddd0f7673e876039caaac49cb8ad90d
NENEPE <oleksandrprystavsky@gmail.com>
2026-06-25 14:05:22 +0200

    Add Participant List

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const regForm = document.querySelector("#regForm");
 const result = document.querySelector("#message");
 
+let participantsArray = [];
+const participantList = document.querySelector("#participants ol");
+const participantCount = document.querySelector("#participants h3");
+
+renderParticipants();
+
 function errorMessage(message) {
     result.textContent = message;
     result.style.color = "red";
@@ -16,6 +22,7 @@ function successMessage(name, field, age) {
     result.style.backgroundColor = "rgba(0, 255, 0, 0.5)";
     result.style.border = "1px solid #6cff6c";
     result.style.borderRadius = "5px";
+    regForm.reset();
 }
 
 regForm.addEventListener("submit", function(event) {
@@ -27,24 +34,20 @@ regForm.addEventListener("submit", function(event) {
     const terms = document.getElementById("terms");
 
     const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const ageValue = parseInt(age.value.trim());
+
     if (nameValue === "") {
         errorMessage("Please enter your full name");
         name.focus();
         return;
     }
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email.value.trim())) {
+    if (!emailPattern.test(emailValue)) {
         errorMessage("Email is invalid");
         email.focus();
         return;
     }
-    if (!emailPattern.test(email.value.trim())) {
-        errorMessage("Please enter a valid email address");
-        email.focus();
-        return;
-    }
-    
-    const ageValue = parseInt(age.value.trim());
     if (isNaN(ageValue) || ageValue < 18) {
         errorMessage("Please enter a valid age (at least 18)");
         age.focus();
@@ -61,5 +64,32 @@ regForm.addEventListener("submit", function(event) {
         return;
     }
 
+    createParticipant(nameValue, field.value, ageValue);
     successMessage(nameValue, field.value, ageValue);
 });
+
+function createParticipant(name, field, age) {
+
+    let participant = {
+        name: name,
+        field: field,
+        age: age
+    };
+
+    participantsArray.push(participant);
+
+    renderParticipants();
+}
+
+function renderParticipants() {
+    participantList.innerHTML = "";
+
+    participantsArray.forEach(participant => {
+        const participantItem = document.createElement("li");
+        participantItem.textContent = `${participant.name} (${participant.age}) - ${participant.field}`;
+        participantList.appendChild(participantItem);
+    });
+
+    participantCount.textContent = `Currently there are ${participantsArray.length} participant(s)`;
+}
+
